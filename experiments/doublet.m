93e50e7963bc5aa5193ee4ec0fe1c28dfd2731f4
Skillbert <wilbert.terhaar@gmail.com>
2022-09-23 15:47:08 +0200

    doublet

--- experiments/doublet.m
+++ experiments/doublet.m
@@ -5,22 +5,22 @@ timesraw=0:h:T;
 
 signaltime=1;
 
-amplitude=0.02;
+amplitude=0.05;
 
-u=zeros(length(timesraw));
+u=zeros(length(timesraw),1);
 
-for i=1:length(u)
+for i=1:length(u)-1
     samplet=timesraw(i);
     duty=mod(samplet,signaltime);
-    if duty<0.05
+    if duty<0.07
         u(i)=amplitude;
-    elseif duty<0.10
+    elseif duty<0.14
         u(i)=-amplitude;
     else
         u(i)=0;
     end
 end
 
-simin=[timesraw',u',zeros(size(u))'];
+simin=[timesraw',u];
 
 plot(timesraw,u)
