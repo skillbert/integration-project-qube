47907caa3187db1e7f025ea9cebba816c41657fd
Skillbert <wilbert.terhaar@gmail.com>
2022-10-13 18:02:50 +0200

    simulink models

--- experiments/refsquare.m
+++ experiments/refsquare.m
@@ -1,14 +1,12 @@
 
-h=0.005;
-T=15;
 timesraw=0:h:T;
 
 steptime=6;
 
-amplitude=0.5;
+amplitude=0.3;
 u=[
     amplitude*square(1/steptime*timesraw*2*pi)
     zeros(3,length(timesraw))
 ];
-u(:,1/h*2)
+u(:,1:1/h*2)=0;
 simin=[timesraw',u'];
