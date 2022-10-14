fa68baa0b3077d4ca11225712433189fffb0e923
skillbert <wilbert.terhaar@gmail.com>
2022-10-14 14:54:19 +0200

    merge

--- observer/makeobserver.m
+++ observer/makeobserver.m
@@ -5,25 +5,21 @@ h=0.005;
 
 sys_pi=ss(sys_0.A.*[1,1,1,1;1,1,1,1;1,1,1,-1;-1,-1,-1,1],diag([1,1,1,-1])*sys_0.B,sys_0.C,sys_0.D);
 
-discrete_0=c2d(sys_pi,h);
+sys_eq=sys_0;
 
-[K,S,e] = dlqr(discrete_0.A,discrete_0.B,diag([20,5,1,1]),0.1);
-K
-% K(1)=-K(1);
+discrete_eq=c2d(sys_eq,h);
 
-% poles =log(e)/h;
+[K,S,e] = dlqr(discrete_eq.A,discrete_eq.B,diag([20,5,1,1]),0.1);
+K;
+
+poles =log(e)/h;
 % pDisc = real(exp(2*poles*h));
 % pDisc(4)=0.5;
 
 pCont=[-80,-81,-60,-61];%poles in s-domain
 pDisc=exp(pCont.*h);%poles in z-domain
 % cont=place(sys_0.A',sys_0.C',pCont)'
-disc=place(discrete_0.A',discrete_0.C',pDisc)'
-
-KK=ss([],[],[],K,h);
-fs=ss(discrete_0.A,discrete_0.B,eye(4),[],h);
-constgain=ones(1,4)./dcgain(feedback(fs,KK))';
-constgain(2:4)=0;
+disc=place(discrete_eq.A',discrete_eq.C',pDisc)'
 
 
 
