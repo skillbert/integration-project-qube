e284a3b989396f7005df403e57e7d4a05e426595
skillbert <wilbert.terhaar@gmail.com>
2022-10-26 12:54:41 +0200

    demean plot

---
+++ reportplots/identification/detrend.m
@@ -0,0 +1,18 @@
+
+global datasetname
+datasetname='20oct';
+stuff = loadexp('sweep') ; 
+validation_stuff= loadexp('sweep') ;
+
+starttime = stuff.h;
+startingnumber = floor(starttime / stuff.h) ;
+
+% y = [detrend(stuff.alpha(2:end)),stuff.theta(2:end) ] ; %output
+y = [stuff.alpha(2:end)-mean(stuff.alpha(2:end)),stuff.theta(2:end) ] ; %output
+
+plot(stuff.time,stuff.alpha,'-',stuff.time,ones(size(stuff.alpha))*mean(stuff.alpha),'--');
+
+legend '\alpha raw data' mean
+
+folder = fileparts(which(mfilename));
+saveas(gcf,[folder,'/demean.eps']);
